6245dd30d5d14b0eef77022f7ee403a46b84f39d
Luis Pablo <luispablo.gallo@gmail.com>
2012-04-24 20:13:43 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Panialera::Application.config.secret_token = '7b9a9b50124f54501d6a30117f30c21ca60f81132241fa934c95717cc1b4accb3add21cb114d51866d5270dbc8be187e224a97bf5ac638582e6978b2486d4bd4'
