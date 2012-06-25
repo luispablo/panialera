bfc3c89780351f8bc82e00a07fea484534ad2865
Luis Pablo <luispablo.gallo@gmail.com>
2012-06-25 20:19:24 -0300

    Corregir el funcionamiento del filtro por categoría.

--- app/models/categoria.rb
+++ app/models/categoria.rb
@@ -16,9 +16,38 @@ class Categoria < ActiveRecord::Base
   belongs_to :categoria
   
   has_many :cat_productos
+  has_many :categorias
   has_many :productos, :through => :cat_productos
 
   validates :codigo, uniqueness: true, presence: true
+  
+  def todos_productos
+    productos = self.productos
+    
+    categorias.each do |hija|
+      productos += hija.todos_productos
+    end
+    
+    return productos
+  end
+  
+  def seleccionada?(sel)
+    return (not sel.nil? and (id == sel.id))
+  end
+  
+  def hija_seleccionada?(categoria)
+    unless categoria.nil?
+      unless categorias.nil? or categorias.empty?
+        categorias.each do |hija|
+          if hija.id == categoria.id or hija.hija_seleccionada?(categoria)
+            return true
+          end
+        end
+      end      
+    end
+    
+    return false
+  end
   
   def self.categorias_madre_publicadas
     Categoria.where(publicado: true, categoria_id: nil)
