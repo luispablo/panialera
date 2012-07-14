10eed7865380d5054d9b240fe2e928068ef23f5d
Luis Pablo <luispablo.gallo@gmail.com>
2012-07-14 18:31:32 -0300

    Proyecto capificado!

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+set :application, "panialera"
+set :repository,  "git@github.com:luispablo/panialera.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :deploy_to, "/opt/apps"
+
+server "li506-126.members.linode.com", :app, :web, :db, :primary => true
+
+# Usuario para loguearse a linode.com
+set :user, "luispablo"
+# Usuario para trabajar con github
+set :scm_username, "luispablo"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
