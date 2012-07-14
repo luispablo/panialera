176a476fdbeb4433c214ef68942497fdf4a69543
Luis Pablo <luispablo.gallo@gmail.com>
2012-07-14 20:48:53 -0300

    Corregir migración.

--- db/migrate/20120630173709_remove_usuario_from_compras.rb
+++ db/migrate/20120630173709_remove_usuario_from_compras.rb
@@ -1,6 +1,6 @@
 class RemoveUsuarioFromCompras < ActiveRecord::Migration
   def change    
-    remove_column :compras, :usuario
-    remove_column :compras, :domicilio
+    remove_column :compras, :usuario_id
+    remove_column :compras, :domicilio_id
   end
 end
