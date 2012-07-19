2dcf309c460f9bef04770676e5e55ad53af8bda4
Luis Pablo <luispablo.gallo@gmail.com>
2012-07-18 21:42:13 -0300

    Agregar más precompilaciones.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Panialera::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( bootstrap.css panialera.css jquery.blockUI.js admin.css )
+  config.assets.precompile += %w( bootstrap.css bootstrap.min.js jquery-ui-1.8.20.custom.css panialera.css jquery.blockUI.js admin.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
