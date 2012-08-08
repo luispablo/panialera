6d1c42e2f8a63c6673c475b02a85ab3f383f8ac9
Luis Pablo <luispablo.gallo@gmail.com>
2012-08-07 21:08:18 -0300

    Ampliar el detalle, no la descripción.

--- db/migrate/20120807231053_change_data_type_for_productos_descripcion.rb
+++ db/migrate/20120807231053_change_data_type_for_productos_descripcion.rb
@@ -1,7 +1,7 @@
 class ChangeDataTypeForProductosDescripcion < ActiveRecord::Migration
   def change
   	change_table :productos do |t|
-  		t.change :descripcion, :text
+  		t.change :detalle, :text
   	end
   end
 end
