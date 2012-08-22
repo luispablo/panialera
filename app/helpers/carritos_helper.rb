8bbff0e4bea48253b68a02ebc49b806c71d15723
Luis Pablo <luispablo.gallo@gmail.com>
2012-08-22 10:07:25 -0300

    Arreglar un error que llegó por mail.

--- app/helpers/carritos_helper.rb
+++ app/helpers/carritos_helper.rb
@@ -1,7 +1,7 @@
 module CarritosHelper
 
 	def puede_comprar?(item)
-		(item.stock_disponible - @carrito.cantidad_agregada(item)) > 0
+		(item.stock_disponible - (@carrito.nil? ? 0 : @carrito.cantidad_agregada(item))) > 0
 	end
 
 end
