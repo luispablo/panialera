c09ed600bee6bad0efd15132435b884377907dce
Luis Pablo <luispablo.gallo@gmail.com>
2012-09-10 21:03:56 -0300

    Issue #279: Pintar de rojo los vales usados.

--- app/models/vale.rb
+++ app/models/vale.rb
@@ -11,22 +11,10 @@
 #  cantidad_usos_disponibles :integer
 #
 
-# == Schema Information
-#
-# Table name: vales
-#
-#  id                        :integer         not null, primary key
-#  codigo                    :string(255)
-#  nombre                    :string(255)
-#  created_at                :datetime        not null
-#  updated_at                :datetime        not null
-#  porcentaje_descuento      :float
-#  cantidad_usos_disponibles :integer
-#
 class Vale < ActiveRecord::Base
 
 	attr_accessor :valor_calculado
-  belongs_to :usuario
+	belongs_to :usuario
   
   validates :codigo, presence: true, uniqueness: true
   validates :porcentaje_descuento, presence: true 
